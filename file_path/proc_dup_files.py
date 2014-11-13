b67c68d6ab02fea7cb763cdde87750702a807061
Dainese Hsiao <daineseh@gmail.com>
2014-11-13 21:34:21 +0800

    [file_path] Add proc_dup_files.py

---
+++ file_path/proc_dup_files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+import os
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print 'Input a directory.'
+        return False
+
+    if not os.path.isdir(sys.argv[1]):
+        print 'Invalid directory - %s' % sys.argv[1]
+
+    name_list = []
+    duplicate_list = []
+    for dir_path, dir_list, file_list in os.walk(sys.argv[1]):
+        for file_name in file_list:
+            if file_name not in name_list:
+                name_list.append(file_name)
+                continue
+            path = os.path.join(dir_path, file_name)
+            duplicate_list.append(path)
+
+    for dup_path in duplicate_list:
+        print dup_path
+
+    if duplicate_list:
+        print '-' * 12
+        print 'Total %s files is duplicated.' % len(duplicate_list)
+        input_str = raw_input('Do you want to remove this files: [Y/N]')
+        for idx, dup in enumerate(duplicate_list, start=1):
+            try:
+                os.remove(dup)
+                print '[%s]%s removed.' % (idx, dup)
+            except OSError, e:
+                    print e
+    else:
+        print 'No duplicate file.'
+
+    return True
+
+
+if __name__ == '__main__':
+    main()
+
