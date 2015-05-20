10487d9eb1ec8ad85d05c713e4780601c2ec996e
Dainese Hsiao <daineseh@gmail.com>
2015-05-21 01:45:31 +0800

    [file_path] Add convert_bytes() in proc_dup_files.py

--- file_path/proc_dup_files.py
+++ file_path/proc_dup_files.py
@@ -3,6 +3,24 @@ import os
 import sys
 
 
+def convert_bytes(bytes):
+    bytes = float(bytes)
+    if bytes >= 1099511627776:
+        terabytes = bytes / 1099511627776
+        size = '%.2fT' % terabytes
+    elif bytes >= 1073741824:
+        gigabytes = bytes / 1073741824
+        size = '%.2fG' % gigabytes
+    elif bytes >= 1048576:
+        megabytes = bytes / 1048576
+        size = '%.2fM' % megabytes
+    elif bytes >= 1024:
+        kilobytes = bytes / 1024
+        size = '%.2fK' % kilobytes
+    else:
+        size = '%sb' % bytes
+    return size
+
 def main():
     if len(sys.argv) < 2:
         print 'Input a directory.'
@@ -13,6 +31,7 @@ def main():
 
     name_list = []
     duplicate_list = []
+    size_count = 0
     for dir_path, dir_list, file_list in os.walk(sys.argv[1]):
         for file_name in file_list:
             lowercase = file_name.lower()
@@ -22,12 +41,16 @@ def main():
             path = os.path.join(dir_path, file_name)
             duplicate_list.append(path)
 
+            statinfo = os.stat(path)
+            size_count += statinfo.st_size
+
     for dup_path in duplicate_list:
-        print dup_path
+        statinfo = os.stat(dup_path)
+        print '%s - %s' % (dup_path, convert_bytes(statinfo.st_size))
 
     if duplicate_list:
         print '-' * 12
-        print 'Total %s files is duplicated.' % len(duplicate_list)
+        print 'Total %s files is duplicated. (%s)' % (len(duplicate_list), convert_bytes(size_count))
         input_str = raw_input('Do you want to remove this files: [Y/N]')
         if input_str.upper() == 'Y':
             for idx, dup in enumerate(duplicate_list, start=1):
